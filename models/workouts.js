b2276ef75566c6d648c1f439cd4f0ccbafa11509
BradMW <bodell94@yahoo.com>
2021-11-13 18:42:11 -0600

    finished models woring on routes now

--- models/workouts.js
+++ models/workouts.js
@@ -3,37 +3,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    date: {
+    day: {
         type: Date,
-        default: Date.now
+        default: Date.now()
     },
-    exercise: [{
-        name: {
-            type: String,
-            required: "Enter a name for the exercise."
-        },
-        type: {
-            type: String,
-            required: "Enter the type of exercise."
-        },
-        weight: {
-            type: String,
-            required: "Enter a name for the exercise."
-        },
-        reps: {
-            type: String,
-            required: "Enter a name for the exercise."
-        },
-        sets: {
-            type: String,
-            required: "Enter a name for the exercise."
-        },
-        duration: Number,
-        distance: {
-            type: String,
-            required: "Enter a name for the exercise."
-        }
-    }]
+    exercises: {
+        type: Array
+    }
+    
 });
 
 const Workouts = mongoose.model("Workouts", workoutSchema);
--- routes/api.js
+++ routes/api.js
@@ -2,54 +2,59 @@ const router = require("express").Router();
 const Workouts = require("../models/workouts.js");
 
 
-router.get("/api/workouts", (req, res) => {
-    Workouts.find({})
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
+router.get("/api/workouts", async (req, res) => {
+  try {
+    let dbWorkout = await Workouts.find({}).sort({day: 1});
+    res.json(dbWorkout);
 
-
-router.get("/api/workouts", (req, res) => {
-  Workouts.find({})
-    .sort({ date: -1 })
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 
-  router.put('/:id', async (req, res) => {
-    try {
-      const categoryData = await Category.update(req.body, {
-        where: {
-          id: req.params.id,
-        },
-      });
-      if (!categoryData[0]) {
-        res.status(404).json({ message: 'No category with this id!' });
-        return;
-      }
-      res.status(200).json(categoryData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+router.get("/api/workouts/range", async (req, res) => {
+  try {                                                                         
+    let dbWorkout = await Workouts.find({}).sort({date: -1})
+                                                                                   
 
-router.post("/api/workouts/bulk", ({ body }, res) => {
-  Workouts.insertMany(body)
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
+    res.json(dbWorkout);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+router.put('/api/workouts/:id', async (req, res) => {
+  try {
+    const workoutData = await Workouts.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
     });
+    if (!workoutData[0]) {
+      res.status(404).json({
+        message: 'No workout with this id!'
+      });
+      return;
+    }
+    res.status(200).json(workoutData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/api/workouts/bulk", async ({
+  body
+}, res) => {
+  try {
+    let dbWorkout = await Workouts.insertMany(body)
+
+    res.json(dbWorkout);
+  } catch (err) {
+
+    res.status(400).json(err);
+  }
 });
 
 
